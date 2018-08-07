4bd6cacbdbad2fe3115926ef63d8da1e5b77b078
github username <github email address>
2018-08-07 16:30:26 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  blurHandler = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  focusHandler = () => {
+    console.log('Good!')
+  }
+
+  render(){
+    return <button onBlur={this.blurHandler} onFocus={this.focusHandler}></button>
+  }
+
+}// Code EyesOnMe Component Here
